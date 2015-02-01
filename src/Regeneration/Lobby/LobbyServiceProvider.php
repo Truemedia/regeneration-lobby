1ebe69b29f45a08ef62e1e18a8a8143565f6fb00
Wade Penistone <truemedia@mediacityonline.net>
2015-02-01 18:07:47 +0000

    Initialized laravel package

---
+++ src/Regeneration/Lobby/LobbyServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Regeneration\Lobby;
+
+use Illuminate\Support\ServiceProvider;
+
+class LobbyServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('regeneration/lobby');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
